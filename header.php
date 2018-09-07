ca3b75f14fb3df831c76c070b9b25dff3c63199d
David <david@usehelvetica.net>
2018-09-07 19:00:34 +0200

    Añadido Menu y tipografía Open Sans

--- header.php
+++ header.php
@@ -21,13 +21,44 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/main.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
 
+    <!-- AOS LIBRARY -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
     <!-- Fontawsome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
 
     <!-- Automatic Wordpress ////////////////////////////////// -->
     <?php wp_head(); ?>
     <!-- Automatic Wordpress ////////////////////////////////// -->
-    
+
   </head>
 
 <body <?php body_class(); ?>>
+
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
+    <a class="navbar-brand" href="#"><?php bloginfo('name'); ?></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <?php
+  		wp_nav_menu( array(
+  			'theme_location'    => 'primary',
+  			'depth'             => 2,
+  			'container'         => 'div',
+  			'container_class'   => 'collapse navbar-collapse',
+  			'container_id'      => 'navbarSupportedContent',
+  			'menu_class'        => 'nav navbar-nav',
+  			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+  			'walker'            => new WP_Bootstrap_Navwalker(),
+  		) );
+  		?>
+    </div>
+  </nav>
+</header>
