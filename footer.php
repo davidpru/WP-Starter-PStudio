37c9e4e35f47d58e892dbf8febccf3673b3c25f2
David <david@usehelvetica.net>
2018-09-28 19:02:51 +0200

    Añadido html básico

--- footer.php
+++ footer.php
@@ -1,6 +1,38 @@
+<aside class="padding">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <?php if (!dynamic_sidebar('Footer 1')); ?>
+      </div>
+      <div class="col-md-4">
+        <?php if (!dynamic_sidebar('Footer 2')); ?>
+      </div>
+      <div class="col-md-4">
+        <?php if (!dynamic_sidebar('Footer 3')); ?>
+      </div>
+      <div class="col-md-4">
+        <?php if (!dynamic_sidebar('Footer 4')); ?>
+      </div>
+    </div>
+  </div>
+</aside>
+
+
+<footer class="bg-light pt-4 pb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <p>Copyright - <strong><?php bloginfo('title') ?></strong></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
 <!-- Wp automatic footer -->
 <?php wp_footer(); ?>
 <!-- Wp automatic footer -->
+
+
 
 <!-- Jquery + Bootstrap -->
 <script src="<?php bloginfo('stylesheet_directory'); ?>/js/jquery/jquery-3.3.1.slim.min.js" ></script>
@@ -12,6 +44,8 @@
 
 <!-- FX AOS.js -->
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
 
 <script>
   AOS.init();
--- page.php
+++ page.php
@@ -1,5 +1,30 @@
 <?php get_header(); ?>
+<?php get_template_part('components/page-title'); ?>
 
+<main class="py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post(); ?>
+            <article class="post" id="post-<?php the_ID(); ?>">
+              <div class="entry">
+                  <?php the_content('Read the rest of this entry &raquo;'); ?>
+              </div>
+            </article>
+          <?php endwhile; ?>
+            <div class="navigation">
+                <div class="alignleft"><?php next_posts_link('&laquo; Previous Entries') ?></div>
+                <div class="alignright"><?php previous_posts_link('Next Entries &raquo;') ?></div>
+            </div>
+        <?php else : ?>
+        <?php endif; ?>
+
+      </div>
+    </div>
+  </div>
+</main>
 
 
 <?php get_footer(); ?>
