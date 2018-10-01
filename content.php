c2e04b146190e1ee3607871aa36d7d82f1b6b3d8
David <david@usehelvetica.net>
2018-10-01 19:03:20 +0200

    añadido footer al content

--- content.php
+++ content.php
@@ -1,5 +1,19 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
   <div class="entry">
-      <?php the_content('Read the rest of this entry &raquo;'); ?>
+      <?php the_content('Leer más &raquo;'); ?>
   </div>
+
+  <footer class="entry-meta">
+    <?php if ( 'post' == $post->post_type ) : ?>
+      <span class="cat-links">En <?php the_category( ', ' ); ?></span>
+      <?php the_tags( ', ' ); ?>
+    <?php endif; ?>
+
+    <?php if ( comments_open() ) : ?>
+      <span class="comments-link"><?php comments_popup_link( ) ?></span>
+    <?php endif; ?>
+
+    <?php edit_post_link( __( 'Edit') ); ?>
+  </footer> <?php ?>
+
 </article>
