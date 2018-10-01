2a6740e82f6dd1df6815d3e1469296ea86102b73
David <david@usehelvetica.net>
2018-10-01 18:53:51 +0200

    creado archivo content.php

---
+++ content.php
@@ -0,0 +1,5 @@
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+  <div class="entry">
+      <?php the_content('Read the rest of this entry &raquo;'); ?>
+  </div>
+</article>
--- page.php
+++ page.php
@@ -8,11 +8,9 @@
 
         <?php if (have_posts()) : ?>
           <?php while (have_posts()) : the_post(); ?>
-            <article class="post" id="post-<?php the_ID(); ?>">
-              <div class="entry">
-                  <?php the_content('Read the rest of this entry &raquo;'); ?>
-              </div>
-            </article>
+
+            <?php get_template_part('content'); ?>
+
           <?php endwhile; ?>
             <div class="navigation">
                 <div class="alignleft"><?php next_posts_link('&laquo; Previous Entries') ?></div>
