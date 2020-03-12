fdeab82e78ea20f3f9d6f981576c78d7284c2e7a
David Pruñonosa <david@usehelvetica.net>
2020-03-12 16:44:20 +0100

    scss add variables

--- header.php
+++ header.php
@@ -32,12 +32,13 @@
 
 <header>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark" role="navigation">
-    <a class="navbar-brand" href="#"><?php bloginfo('name'); ?></a>
+    <a class="navbar-brand" href="<?php bloginfo('name') ?>">
+      <?php bloginfo('name'); ?>
+    </a>
+
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
       <?php
   		wp_nav_menu( array(
@@ -51,6 +52,7 @@
   			'walker'            => new WP_Bootstrap_Navwalker(),
   		) );
   		?>
-    </div>
+
+
   </nav>
 </header>
